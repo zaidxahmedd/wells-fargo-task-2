70dec5bb2518e3271c11c3e8f4d18af62089f50f
zaidxahmedd <zaidxahmedd@gmail.com>
2025-10-31 17:05:24 +0530

    Create Portfolio.java

---
+++ src/main/java/com/wellsfargo/counselor/entity/Portfolio.java
@@ -0,0 +1,36 @@
+package com.wellsfargo.counselor.entity;
+
+import jakarta.persistence.*;
+import java.util.List;
+
+@Entity
+public class Portfolio {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @OneToOne
+    @JoinColumn(name = "client_id")
+    private Client client;
+
+    @OneToMany(mappedBy = "portfolio", cascade = CascadeType.ALL)
+    private List<Security> securities;
+
+    public Portfolio() {}
+
+    public Portfolio(String name, Client client) {
+        this.name = name;
+        this.client = client;
+    }
+
+    public Long getId() { return id; }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+    public Client getClient() { return client; }
+    public void setClient(Client client) { this.client = client; }
+    public List<Security> getSecurities() { return securities; }
+    public void setSecurities(List<Security> securities) { this.securities = securities; }
+}
