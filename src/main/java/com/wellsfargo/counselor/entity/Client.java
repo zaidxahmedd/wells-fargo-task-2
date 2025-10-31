3459dce41cec683f5d38fefb771cba8f8c63457c
zaidxahmedd <zaidxahmedd@gmail.com>
2025-10-31 17:04:04 +0530

    Update Client.java

--- src/main/java/com/wellsfargo/counselor/entity/Client.java
+++ src/main/java/com/wellsfargo/counselor/entity/Client.java
@@ -1,1 +1,34 @@
+package com.wellsfargo.counselor.entity;
 
+import jakarta.persistence.*;
+
+@Entity
+public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
+
+    @ManyToOne
+    @JoinColumn(name = "advisor_id")
+    private Advisor advisor;
+
+    public Client() {}
+
+    public Client(String name, String email, Advisor advisor) {
+        this.name = name;
+        this.email = email;
+        this.advisor = advisor;
+    }
+
+    public Long getId() { return id; }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+    public Advisor getAdvisor() { return advisor; }
+    public void setAdvisor(Advisor advisor) { this.advisor = advisor; }
+}
